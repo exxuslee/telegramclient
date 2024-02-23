efa91412c80a51e85e15e656c7490555d13904d6
Exxus Neo <exxuslee@gmail.com>
2024-02-23 17:48:55 +0200

    init

--- start.js
+++ start.js
@@ -1,14 +1,28 @@
 require("dotenv").config();
-const {TelegramClient}  = require('telegram')
-const {StringSession}  = require('telegram/sessions')
+const {TelegramClient} = require('telegram')
+const {StringSession} = require('telegram/sessions')
 
 const apiId = parseInt(process.env.TELEGRAM_APIID);
 const apiHash = process.env.TELEGRAM_APIHASH;
 const stringSession = new StringSession(process.env.SESSION);
-(async () => {
-    console.log("Loading interactive example...");
-    const client = new TelegramClient(stringSession, apiId, apiHash, {connectionRetries: 5,});
-    await client.connect()
+const client = new TelegramClient(stringSession, apiId, apiHash, {connectionRetries: 5,});
+let lastMessage = 0
 
-    await client.sendMessage('me', { message: "Hello!" });
+
+async function start(coin, side) {
+    console.log(coin, side)
+}
+
+async function read() {
+    let messages = await client.getMessages("invest_zonaa", {limit: 1})
+    if (messages[0].time === lastMessage) return
+    lastMessage = messages[0].time
+    let text = messages.map((message) => message.message.split("\n").filter(item => item.trim() !== ''))[0]
+    if (text[1] === "Long") await start(text[0], "BUY")
+    if (text[1] === "Sort") await start(text[0], "SELL")
+}
+
+(async () => {
+    await client.connect()
+    setInterval(read, 15000)
 })();
