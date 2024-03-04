93af21db70afbfe2698dac48b7f7e6beb6ac3443
Exxus Neo <exxuslee@gmail.com>
2024-03-04 14:29:25 +0200

    init

--- start.js
+++ start.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const {TelegramClient} = require('telegram')
+const { NewMessage } = require('telegram/events')
 const {StringSession} = require('telegram/sessions')
 
 const apiId = parseInt(process.env.TELEGRAM_APIID);
@@ -13,16 +14,19 @@ async function start(coin, side) {
     console.log(coin, side)
 }
 
-async function read() {
-    let messages = await client.getMessages("invest_zonaa", {limit: 1})
-    if (messages[0].time === lastMessage) return
-    lastMessage = messages[0].time
-    let text = messages.map((message) => message.message.split("\n").filter(item => item.trim() !== ''))[0]
-    if (text[1] === "Long") await start(text[0], "BUY")
-    if (text[1] === "Sort") await start(text[0], "SELL")
+async function eventPrint(event) {
+    const message = event.message;
+    if (event.isPrivate) {
+        let text = message.message.split("\n").filter(item => item.trim() !== '')
+        if (text[1] === "Long") await start(text[0], "BUY")
+        if (text[1] === "Sort") await start(text[0], "SELL")
+        console.log(text)
+    }
 }
 
 (async () => {
     await client.connect()
-    setInterval(read, 15000)
+    client.addEventHandler(async (event) => {
+        eventPrint(event);
+    }, new NewMessage({ incoming: true, channels: ['invest_zonaa'] }));
 })();
